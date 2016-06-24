2650fa2abf430b0dc6c2aa2aecc53058d392853b
Matias Mansilla <tute.mdm@gmail.com>
2016-06-23 21:51:19 -0300

    superclase ObjetoEspacial y clase Nave

---
+++ model/Nave.rb
@@ -0,0 +1,3 @@
+class Nave < ObjetoEspacial
+
+end
---
+++ model/ObjetoEspacial.rb
@@ -0,0 +1,19 @@
+class ObjetoEspacial
+
+  @vida
+  @masa
+  
+  def initialize(vida, masa)     
+    @vida = vida    
+    @masa = masa
+  end
+
+  def vida
+    @vida
+  end
+
+  def masa
+    @masa
+  end     
+
+end
