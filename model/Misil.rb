86f0c763b5d4d5658a859da643741a9527319ca2
Matias Mansilla <tute.mdm@gmail.com>
2016-06-23 22:41:28 -0300

    Primera version de Misil

---
+++ model/Misil.rb
@@ -0,0 +1,3 @@
+class Misil < ObjetoEspacial
+
+end
---
+++ spec/Misil_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec' 
+require_relative '../model/ObjetoEspacial'
+require_relative '../model/Misil'
+
+describe 'Misil' do
+
+  it 'debe crear un misil con 10 unidades de vida y 30 unidades de masa' do
+    
+    vidaEsperada = 10
+    masaEsperada = 30
+    misil = Misil.new(vidaEsperada, masaEsperada) 
+    
+    vidaObtenida = misil.vida
+    masaObtenida = misil.masa    
+
+    expect(vidaEsperada).to eq vidaObtenida
+    expect(masaEsperada).to eq masaObtenida
+  end
+
+end
