be9cb0f7c2fcfaa1d4c5cd827f94259d64fc2e7d
Matias Mansilla <tute.mdm@gmail.com>
2016-06-24 16:49:05 -0300

    Nuevo test en integracion_spec

--- spec/integracion_spec.rb
+++ spec/integracion_spec.rb
@@ -2,9 +2,33 @@ require 'rspec'
 require_relative '../model/nave'
 require_relative '../model/asteroide'
 
-describe 'Tipos de choque' do
+describe 'Distintos tipos de choque' do
 
-it 'Nave con asteroide: la nave disminuye su masa a 50 unidades y el asteroide la aumenta en 10' do
+  it 'Nave con nave: la dos naves disminuyen su vida en 100 unidades' do
+    
+    vida_nave = 100
+    masa_nave = 100
+    nave_que_choca = Nave.new(vida_nave, masa_nave) 
+    nave_chocada = Nave.new(vida_nave, masa_nave) 
+    vida_nave_que_choca_esperada = 0
+    masa_nave_que_choca_esperada = 100
+    vida_nave_chocada_esperada = 0
+    masa_nave_chocada_esperada = 100
+
+    nave_que_choca.chocar(nave_chocada)
+    
+    vida_nave_que_choca_obtenida = nave_que_choca.vida
+    masa_nave_que_choca_obtenida = nave_que_choca.masa    
+    vida_nave_chocada_obtenida = nave_chocada.vida
+    masa_nave_chocada_obtenida = nave_chocada.masa     
+
+    expect(vida_nave_que_choca_obtenida).to eq vida_nave_que_choca_esperada
+    expect(masa_nave_que_choca_obtenida).to eq masa_nave_que_choca_esperada
+    expect(vida_nave_chocada_obtenida).to eq vida_nave_chocada_esperada
+    expect(masa_nave_chocada_obtenida).to eq masa_nave_chocada_esperada    
+  end
+
+  it 'Nave con asteroide: la nave disminuye su masa a 50 unidades y el asteroide la aumenta en 10' do
     
     vida_nave = 100
     masa_nave = 100
@@ -18,6 +42,32 @@ it 'Nave con asteroide: la nave disminuye su masa a 50 unidades y el asteroide l
     masa_asteroide_esperada = 110
 
     nave.chocar(asteroide)
+    
+    vida_nave_obtenida = nave.vida
+    masa_nave_obtenida = nave.masa    
+    vida_asteroide_obtenida = asteroide.vida
+    masa_asteroide_obtenida = asteroide.masa    
+
+    expect(vida_nave_obtenida).to eq vida_nave_esperada
+    expect(masa_nave_obtenida).to eq masa_nave_esperada
+    expect(vida_asteroide_obtenida).to eq vida_asteroide_esperada
+    expect(masa_asteroide_obtenida).to eq masa_asteroide_esperada    
+  end
+
+  it 'Asteroide con nave: la nave disminuye su masa a 50 unidades y el asteroide la aumenta en 10' do
+    
+    vida_nave = 100
+    masa_nave = 100
+    nave = Nave.new(vida_nave, masa_nave) 
+    vida_asteroide = 50
+    masa_asteroide = 100
+    asteroide = Asteroide.new(vida_asteroide, masa_asteroide) 
+    vida_nave_esperada = 100
+    masa_nave_esperada = 50
+    vida_asteroide_esperada = 50
+    masa_asteroide_esperada = 110
+
+    asteroide.chocar(nave)
     
     vida_nave_obtenida = nave.vida
     masa_nave_obtenida = nave.masa    
