42add56bfd6aaf9ecdb4c2675fb1cc571c1b83de
Matias Mansilla <tute.mdm@gmail.com>
2016-06-24 15:12:18 -0300

    Correccion de errores

--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -1,10 +1,9 @@
 require 'rspec' 
-require_relative '../model/objeto_espacial'
 require_relative '../model/nave'
 
 describe 'Nave' do
 
-  it 'debe crear una nave con 100 unidades de vida y 100 unidades de masa' do
+  it 'deberia crear una nave con 100 unidades de vida y 100 unidades de masa' do
     
     vida_esperada = 100
     masa_esperada = 100
@@ -13,8 +12,8 @@ describe 'Nave' do
     vida_obtenida = nave.vida
     masa_obtenida = nave.masa    
 
-    expect(vida_esperada).to eq vida_obtenida
-    expect(masa_esperada).to eq masa_obtenida
+    expect(vida_obtenida).to eq vida_esperada
+    expect(masa_obtenida).to eq masa_esperada
   end
 
 end
