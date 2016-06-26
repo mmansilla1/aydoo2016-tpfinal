c29406cdf49f190127be8ab537f5319cba861ab5
Matias <tute.mdm@gmail.com>
2016-06-26 20:09:11 -0300

    Correccion de errores

--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -13,7 +13,7 @@ describe 'Misil' do
     vida_obtenida = misil.vida
     masa_obtenida = misil.masa    
 
-    expect(masa_esperada).to eq vida_obtenida
+    expect(vida_esperada).to eq vida_obtenida
     expect(masa_esperada).to eq masa_obtenida
   end
 
