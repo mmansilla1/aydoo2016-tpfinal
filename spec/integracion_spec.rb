5c3134fa21b77216ad5842f2b8d5c30e3f7c947f
Matias <tute.mdm@gmail.com>
2016-06-26 06:34:32 +0200

    Nuevo test de integracion choque asteroide con estrella

--- spec/integracion_spec.rb
+++ spec/integracion_spec.rb
@@ -236,4 +236,30 @@ describe 'Casos de choque' do
     expect(masa_asteroide_obtenida).to eq masa_asteroide_esperada    
   end
 
+  it '10) Asteroide con estrella: El asteroide no registra efectos y la estrella disminuye su vida a 0' do
+    
+    vida_asteroide = 100
+    masa_asteroide = 100
+    asteroide = Asteroide.new(vida_asteroide, masa_asteroide) 
+    vida_estrella = 100
+    masa_estrella = 100
+    estrella = Estrella.new(vida_estrella, masa_estrella) 
+    vida_asteroide_esperada = 100
+    masa_asteroide_esperada = 100
+    vida_estrella_esperada = 0
+    masa_estrella_esperada = 100
+
+    asteroide.chocar(estrella)
+    
+    vida_asteroide_obtenida = asteroide.vida
+    masa_asteroide_obtenida = asteroide.masa    
+    vida_estrella_obtenida = estrella.vida
+    masa_estrella_obtenida = estrella.masa    
+
+    expect(vida_asteroide_obtenida).to eq vida_asteroide_esperada
+    expect(masa_asteroide_obtenida).to eq masa_asteroide_esperada
+    expect(vida_estrella_obtenida).to eq vida_estrella_esperada
+    expect(masa_estrella_obtenida).to eq masa_estrella_esperada    
+  end
+
 end
