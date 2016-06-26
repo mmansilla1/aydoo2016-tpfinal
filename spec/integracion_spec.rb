42c5e876769615c12136ee006fd95b55888d6111
Matias <tute.mdm@gmail.com>
2016-06-26 06:10:23 +0200

    Nuevo test de integracion choque nave con asteroide

--- spec/integracion_spec.rb
+++ spec/integracion_spec.rb
@@ -108,33 +108,7 @@ describe 'Casos de choque' do
     expect(masa_bomba_obtenida).to eq masa_bomba_esperada    
   end
 
-  it '5) Nave con asteroide: la nave disminuye su masa a 50 unidades y el asteroide la aumenta en 10' do
-    
-    vida_nave = 100
-    masa_nave = 100
-    nave = Nave.new(vida_nave, masa_nave) 
-    vida_asteroide = 50
-    masa_asteroide = 100
-    asteroide = Asteroide.new(vida_asteroide, masa_asteroide) 
-    vida_nave_esperada = 100
-    masa_nave_esperada = 50
-    vida_asteroide_esperada = 50
-    masa_asteroide_esperada = 110
-
-    nave.chocar(asteroide)
-    
-    vida_nave_obtenida = nave.vida
-    masa_nave_obtenida = nave.masa    
-    vida_asteroide_obtenida = asteroide.vida
-    masa_asteroide_obtenida = asteroide.masa    
-
-    expect(vida_nave_obtenida).to eq vida_nave_esperada
-    expect(masa_nave_obtenida).to eq masa_nave_esperada
-    expect(vida_asteroide_obtenida).to eq vida_asteroide_esperada
-    expect(masa_asteroide_obtenida).to eq masa_asteroide_esperada    
-  end
-
-  it 'Asteroide con nave: la nave disminuye su masa a 50 unidades y el asteroide la aumenta en 10' do
+  it '5) Asteroide con nave: la nave disminuye su masa a 50 unidades y el asteroide la aumenta en 10' do
     
     vida_nave = 100
     masa_nave = 100
