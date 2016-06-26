20123f8398410d0f49d5e7398071385d925a9851
Matias <tute.mdm@gmail.com>
2016-06-26 16:58:43 -0300

    Se agrega test de excepciones

--- spec/excepciones_spec.rb
+++ spec/excepciones_spec.rb
@@ -1,17 +1,38 @@
 require 'rspec' 
 require 'spec_helper'
-require_relative '../model/objeto_espacial'
+require_relative '../model/ObjetoEspacial'
 
 describe 'Excepciones' do
 
   it 'Deberia lanzar excepcion cuando el objeto que choca esta muerto' do
     
     objeto_espacial_que_choca = ObjetoEspacial.new() 
-    objeto_espacial_que_chocado = ObjetoEspacial.new() 
+    objeto_espacial_chocado = ObjetoEspacial.new() 
     vida = 0
     objeto_espacial_que_choca.vida = vida
 
-    expect{objeto_espacial_que_choca.chocar(objeto_espacial_que_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
+    expect{objeto_espacial_que_choca.chocar(objeto_espacial_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
+  end
+
+  it 'Deberia lanzar excepcion cuando el objeto que es chocado esta muerto' do
+    
+    objeto_espacial_que_choca = ObjetoEspacial.new() 
+    objeto_espacial_chocado = ObjetoEspacial.new() 
+    vida = 0
+    objeto_espacial_que_chocado.vida = vida
+
+    expect{objeto_espacial_que_choca.chocar(objeto_espacial_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
+  end
+
+  it 'Deberia lanzar excepcion cuando ambos objetos estan muertos' do
+    
+    objeto_espacial_que_choca = ObjetoEspacial.new() 
+    objeto_espacial_chocado = ObjetoEspacial.new() 
+    vida = 0
+    objeto_espacial_que_choca.vida = vida    
+    objeto_espacial_chocado.vida = vida
+
+    expect{objeto_espacial_que_choca.chocar(objeto_espacial_chocado)}.to raise_exception(ObjetoEspacialMuertoException)
   end
 
 end
